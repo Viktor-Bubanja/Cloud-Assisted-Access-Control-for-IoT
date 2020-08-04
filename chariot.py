85fccdcaed276198740ebd90157e6cc55698ffb0
Viktor Bubanja <viktor.bubanja@hotmail.com>
2020-08-04 13:11:06 +1200

    Implement Verify algorithm

--- chariot.py
+++ chariot.py
@@ -18,6 +18,8 @@ UTF = 'utf-8'
 
 
 class Chariot:
+    s, t = 0, 0
+    p = 29  # TODO change to actual prime
 
     def __init__(self, group):
         super().__init__()
@@ -94,10 +96,12 @@ class Chariot:
                 SecretKey(K))
 
     def request(self, signing_policy, private_key):
-        t, policy = signing_policy
+        # Need to store t and s on the IoT device to perform the Sign algorithm later.
+        self.t, policy = signing_policy
+        self.s = len(policy)
         K = private_key.K
         policy = set([hmac.new(bytes(str(K), UTF), bytes(at, UTF), HMAC_HASH_FUNC).digest() for at in policy])
-        return ThresholdPolicy(threshold=t, policy=policy)
+        return ThresholdPolicy(threshold=self.t, policy=policy)
 
     def sign_out(self, params: PublicParams, osk: OutsourcingKey, threshold_policy: ThresholdPolicy):
         s = len(threshold_policy.policy)
@@ -109,28 +113,28 @@ class Chariot:
         common_attributes = common_attributes[:t]
         T1 = aggregate(osk.g1, list(osk.hashed_attributes))
         remaining_attributes = [at for at in threshold_policy.policy if at not in common_attributes]
+        # TODO calculate HMAC of attributes
         T2_b_coefficients = get_polynomial_coefficients(remaining_attributes)
 
         T2_dash = osk.h2
         for i in range(s - t):
             T2_dash *= osk.h1[i + params.n - s + t] ** T2_b_coefficients[i]
 
-        Hs_b_coefficients = get_polynomial_coefficients(threshold_policy.policy)
-        Hs = 1
-        for i in range(s):
-            Hs *= params.hi[i] ** Hs_b_coefficients[i]
+        # TODO calculate HMAC of attributes
+        Hs = calculate_H_polynomial(threshold_policy.policy, params.hi)
 
-        equality_term1 = self.group.pair(T2_dash, 1 / (params.vi[params.n - s + t - 1]))
-        equality_term2 = self.group.pair(T1, Hs)
-        equality_term3 = self.group.pair(params.u * osk.g2, params.hi[s - t - 1])
-        if equality_term1 * equality_term2 != equality_term3:
+        equality_term1 = self.group.pair(T1, Hs)
+        equality_term2 = self.group.pair(params.u * osk.g2, params.hi[s - t - 1])
+        equality_term3 = self.group.pair(T2_dash, params.vi[params.n - s + t - 1])
+
+        if equality_term1 != chain_multiply([equality_term2, equality_term3], self.p):
             return None
 
         r1, s1, r2, s2 = self.group.random(), self.group.random(), self.group.random(), self.group.random()
         r_theta, s_theta = self.group.random(), self.group.random()
 
-        C_T1_dash = Commitment(r1, s1, T1, params)
-        C_T2_dash = Commitment(r2, s2, T2_dash, params)
+        C_T1_dash = Commitment(r1, s1, T1, params.g1, params.g2)
+        C_T2_dash = Commitment(r2, s2, T2_dash, params.g1, params.g2)
 
         pi_1_dash = Vector([
             Hs ** r1,
@@ -148,9 +152,7 @@ class Chariot:
         g_r = osk.g2 ** r_theta
         g_s = osk.g2 ** s_theta
 
-        theta = params.hi[s - t - 1]
-
-        C_theta_dash = Commitment(self.group.random(), self.group.random(), params.g1, params.g2, theta)
+        C_theta_dash = Commitment(self.group.random(), self.group.random(), params.hi[s - t - 1], params.g1, params.g2)
 
         return OutsourcedSignature(
             C_T1_dash=C_T1_dash,
@@ -172,11 +174,10 @@ class Chariot:
         T2 = outsourced_signature.T2_dash * sk.h
         T1 = outsourced_signature.C_T1_dash
 
-        # TODO where to get vi[n - s + t] from?
-        equality_term1 = self.group.pair(T2, params.vi[params.n - s + t - 1])
+        equality_term1 = self.group.pair(T2, params.vi[params.n - self.s + self.t - 1])
         equality_term2 = self.group.pair()
 
-        # The hashed
+        # TODO The update function returns k bytes, not k bits
         hashed_message = params.hash_function.update(message).digest()
 
         g_3_m = Vector([params.g3[0][0], params.g3[0][1], params.g3[0][2]])
@@ -204,11 +205,72 @@ class Chariot:
             Vector([
                 outsourced_signature.g_r,
                 outsourced_signature.g_s])
-        ) # Unfinished
+        )  # Unfinished
 
         pi_2 = outsourced_signature.pi_2_dash.dot(Vector([1, 1, params.g ** t_theta]))
 
         return Signature(C_T1=C_T1, C_T2=C_T2, C_theta=C_theta, pi_1=pi_1, pi_2=pi_2)
+
+    def verify(self, params: PublicParams, secret_key: SecretKey, message: str, signature: Signature,
+               threshold_policy: ThresholdPolicy):
+        s = len(threshold_policy.policy)
+        t = threshold_policy.threshold
+
+        hashed_message = params.hash_function.update(message).digest()
+
+        g_3_m = Vector([params.g3[0][0], params.g3[0][1], params.g3[0][2]])
+        for mi, gi in zip(hashed_message[1:], params.g3[1:]):
+            g_3_m = g_3_m.dot(Vector(gi).power(mi))
+
+        # TODO HMAC the policy
+        Hs = calculate_H_polynomial(threshold_policy.policy, params.hi)
+
+        pi_1_1, pi_1_2, pi_1_3 = signature.pi_1.vector
+        pi_2_1, pi_2_2, pi_2_3 = signature.pi_2.vector
+
+        equality_term1_1 = self.group.pair(Hs, signature.C_T1)
+        equality_term1_2 = self.group.pair(params.u, signature.C_theta)
+        equality_term1_3 = self.group.pair(params.vi[params.n - self.s + self.t - 1], signature.C_T2)
+        equality_term1_4 = self.group.pair(pi_1_1, params.g1)
+        equality_term1_5 = self.group.pair(pi_1_2, params.g2)
+        equality_term1_6 = self.group.pair(pi_1_3, g_3_m)
+
+        equality_term2_1 = self.group.pair(params.g, signature.C_theta)
+        equality_term2_2 = self.group.pair(params.g, (1, 1, params.hi[s - t - 1]))
+        equality_term2_3 = self.group.pair(pi_2_1, params.g1)
+        equality_term2_4 = self.group.pair(pi_2_2, params.g2)
+        equality_term2_5 = self.group.pair(pi_2_3, g_3_m)
+
+        if equality_term1_1 == chain_multiply([
+            equality_term1_2,
+            equality_term1_3,
+            equality_term1_4,
+            equality_term1_5,
+            equality_term1_6
+        ], self.p) and equality_term2_1 == chain_multiply([
+            equality_term2_2,
+            equality_term2_3,
+            equality_term2_4,
+            equality_term2_5
+        ], self.p):
+            return 0
+        else:
+            return 1
+
+
+def chain_multiply(nums, p):
+    def modular_multiply(a, b):
+        return a * b % p
+
+    return reduce(modular_multiply, nums, 1)
+
+
+def calculate_H_polynomial(attributes, hi):
+    Hs_b_coefficients = get_polynomial_coefficients(attributes)
+    Hs = 1
+    for i in range(len(attributes)):
+        Hs *= hi[i] ** Hs_b_coefficients[i]
+    return Hs
 
 
 def aggregate(x_array, p_array):
@@ -231,8 +293,7 @@ This function can be used to find the coefficients within the expanded form
 of a polynomial given its factored form.
 Given the list of solutions to the polynomial when it is set to equal 0 
 (i.e. a1 ... an in the factored form above), this function returns the list
-of coefficients within the expanded form (b1 ... bn in the expanded form above)
-"""
+of coefficients within the expanded form (b1 ... bn in the expanded form above)"""
 
 
 def get_polynomial_coefficients(numbers):
