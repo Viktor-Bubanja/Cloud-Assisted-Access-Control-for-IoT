c3d1cb561b23197a88942382f48f21a1064d4ecf
Viktor Bubanja <viktor.bubanja@hotmail.com>
2020-09-04 12:02:29 +1200

    Add missing documentation on Chariot class and several methods within Chariot class

--- chariot.py
+++ chariot.py
@@ -18,19 +18,27 @@ from functools import reduce
 HMAC_HASH_FUNC = 'sha256'
 UTF = 'utf-8'
 
-p = 730750818665451621361119245571504901405976559617
 
-
+"""
+CHARIOT: Cloud-Assisted Access Control for the Internet of Things.
+CHARIOT is a threshold policy-based access control protocol that enables an IoT platform to verify credentials of 
+IoT devices based on their attributes.
+"""
 class Chariot:
     s, t = 0, 0
 
-    def __init__(self, group, k):
+    def __init__(self, group, p, k):
         assert k % 8 == 0
         self.group = group
+        self.p = p
         self.k = k
-        self.infinite_element = self.group.random(G1) ** p
+        self.identity_element = self.group.random(G1) ** p  # The elliptic curve's "point at infinity"
 
-    def setup(self, security_param, attribute_universe, n) -> (PublicParams, MasterSecretKey):
+
+    """
+    Responsible for initializing the public parameters of the protocol and the master secret key.
+    """
+    def setup(self, attribute_universe, n) -> (PublicParams, MasterSecretKey):
         # Let g, h be two generators of G.
         g, h = self.group.random(G1), self.group.random(G1)
         alpha, beta, gamma = self.group.random(ZR), self.group.random(ZR), self.group.random(ZR)
@@ -42,19 +50,22 @@ class Chariot:
         generator1 = self.group.random(G1)
         generator2 = self.group.random(G2)
 
-        g1 = Vector([generator1, self.infinite_element, g])
-        g2 = Vector([self.infinite_element, generator2, g])
+        g1 = Vector([generator1, self.identity_element, g])
+        g2 = Vector([self.identity_element, generator2, g])
         g3 = []
 
         for i in range(self.k + 1):
             xi1, xi2 = self.group.random(ZR), self.group.random(ZR)
             g3.append(Vector([generator1 ** xi1, generator2 ** xi2, g ** (xi1 + xi2)]))
 
-        return (PublicParams(security_param=security_param,
-                             attribute_universe=attribute_universe,
+        return (PublicParams(attribute_universe=attribute_universe,
                              n=n, g=g, h=h, u=u, vi=vi, hi=hi, g1=g1, g2=g2, g3=g3),
                 MasterSecretKey(alpha=alpha, beta=beta, gamma=gamma))
 
+
+    """
+    Initializes the outsourcing key, the private key, and the secret key.
+    """
     def keygen(self, params, msk, attributes) -> (OutsourcingKey, PrivateKey, SecretKey):
         K = secrets.SystemRandom().randint(1, 100)  # Secure random key
         beta1 = self.group.random()
@@ -79,6 +90,11 @@ class Chariot:
                 PrivateKey(sk_h1, K),
                 SecretKey(K))
 
+
+    """
+    Given a threshold signing policy and a private key, calculates the HMAC of the attributes within the policy
+    using the private key and returns the hashed threshold policy.
+    """
     def request(self, signing_policy: ThresholdPolicy, private_key: PrivateKey) -> ThresholdPolicy:
         # Need to store t and s on the IoT device to perform the Sign algorithm later.
         self.t = signing_policy.threshold
@@ -89,6 +105,12 @@ class Chariot:
         hashed_policy = set([self.calculate_HMAC(K, at) for at in policy])
         return ThresholdPolicy(threshold=self.t, policy=hashed_policy)
 
+
+    """
+    Given the public parameters, the outsourcing key, and the threshold policy, generates the outsourced signature.
+    By operating on the HMAC-hashed attributes, no information about the attributes is exposed apart from the number
+    of matching attributes between the policy and the device's attributes.
+    """
     def sign_out(self, params: PublicParams, osk: OutsourcingKey,
                  threshold_policy: ThresholdPolicy) -> OutsourcedSignature:
         s = len(threshold_policy.policy)
@@ -128,7 +150,7 @@ class Chariot:
 
         pi_1_dash_1 = (Hs ** r1) * ((params.u * osk.g2) ** -r_theta) * (params.vi[params.n - s + t] ** -r2)
         pi_1_dash_2 = (Hs ** s1) * ((params.u * osk.g2) ** -s_theta) * (params.vi[params.n - s + t] ** -s2)
-        pi_1_dash = Vector([pi_1_dash_1, pi_1_dash_2, self.infinite_element])
+        pi_1_dash = Vector([pi_1_dash_1, pi_1_dash_2, self.identity_element])
 
         pi_2_dash = Vector([params.g ** r_theta, params.g ** s_theta, 1])
 
@@ -149,10 +171,11 @@ class Chariot:
             g_s=g_s
         )
 
-    """
-    :param message is a string of the form bin(integer). i.e. binary.
-    """
 
+    """
+    Using the outsourced signature returned from sign_out and the public parameters, private key, and message, outputs
+    the signature for the IoT device.
+    """
     def sign(self, params: PublicParams, sk: PrivateKey, message: str,
              outsourced_signature: OutsourcedSignature) -> Signature:
         T2 = outsourced_signature.T2_dash * sk.h
@@ -167,11 +190,7 @@ class Chariot:
         if equality_term1 != equality_term2 * equality_term3:
             raise EqualityDoesNotHold
 
-        hashed_message = hash_message(int(self.k / 8), bytes(message, UTF))
-
-        g_3_m = params.g3[0]  # Initial value
-        for mi, gi in zip(hashed_message, params.g3[1:]):
-            g_3_m = g_3_m.dot(gi.exp(int(mi)))
+        g_3_m = self.calculate_g3_m_vector(params.g3, message)
 
         t1, t2, t_theta = self.group.random(), self.group.random(), self.group.random()
 
@@ -198,15 +217,17 @@ class Chariot:
 
         return Signature(C_T1=C_T1, C_T2=C_T2, C_theta=C_theta, pi_1=pi_1, pi_2=pi_2)
 
+
+    """
+    Given the public parameters, secret key, message, IoT device signature, and threshold policy, verifies whether the
+    IoT device should be authenticated.
+    Returns 0 if the device is authenticated.
+    Returns 1 if the device is not authenticated.
+    """
     def verify(self, params: PublicParams, secret_key: SecretKey, message: str, signature: Signature,
                threshold_policy: ThresholdPolicy) -> int:
 
-        hashed_message = hash_message(int(self.k / 8), bytes(message, UTF))
-
-        g_3_m = Vector([params.g3[0][0], params.g3[0][1], params.g3[0][2]])
-
-        for mi, gi in zip(hashed_message, params.g3[1:]):
-            g_3_m = g_3_m.dot(gi.exp(int(mi)))
+        g_3_m = self.calculate_g3_m_vector(params.g3, message)
 
         hashed_policy = set([self.calculate_HMAC(secret_key.K, at) for at in threshold_policy.policy])
 
@@ -214,7 +235,6 @@ class Chariot:
 
         pi_1_1, pi_1_2, pi_1_3 = signature.pi_1.elements
         pi_2_1, pi_2_2, pi_2_3 = signature.pi_2.elements
-
 
         for i in range(3):
             equality_1_left = pair(Hs, signature.C_T1[i])
@@ -225,29 +245,42 @@ class Chariot:
                 pair(pi_1_2, params.g2[i]) *
                 pair(pi_1_3, g_3_m[i])
             )
-            if equality_1_left != equality_1_right:
-                return 1  # Corresponds to failed completion
 
             equality_2_left = pair(params.g, signature.C_theta[i])
-
             equality_2_right = (
-                pair(params.g, self.infinite_element if i != 2 else params.hi[self.s - self.t]) *
+                pair(params.g, self.identity_element if i != 2 else params.hi[self.s - self.t]) *
                 pair(pi_2_1, params.g1[i]) *
                 pair(pi_2_2, params.g2[i]) *
                 pair(pi_2_3, g_3_m[i])
             )
-            if equality_2_left != equality_2_right:
-                return 1  # Corresponds to failed completion
 
-        return 0  # Corresponds to successful completion
+            if equality_1_left != equality_1_right or equality_2_left != equality_2_right:
+                return 1
+
+        return 0
 
 
+    def calculate_g3_m_vector(self, g3, message):
+        hashed_message = hash_message(int(self.k / 8), bytes(message, UTF))
 
+        g_3_m = g3[0]  # Initial value
+        for mi, gi in zip(hashed_message, g3[1:]):
+            g_3_m = g_3_m.dot(gi.exp(int(mi)))
+
+        return g_3_m
+
+
+    """
+    Initializes a Charm ZR element (an int between 0 and Zr) from the HMAC of the given message and secret key.
+    """
     def calculate_HMAC(self, secret_key: int, message: int):
         hashed_message = hmac.new(bytes(secret_key), bytes(message), HMAC_HASH_FUNC).digest()
         return self.group.init(ZR, int.from_bytes(hashed_message, byteorder=sys.byteorder))
 
-
+"""
+Hashes a message (bytes) to the given digest size. Formats each of the returned bytes to contain all eight bits then
+returns a string containing all the bytes.
+"""
 def hash_message(digest_size: int, message: bytes) -> str:
     hash_function = blake2b(digest_size=digest_size)
     hash_function.update(message)
@@ -262,15 +295,12 @@ def hash_message(digest_size: int, message: bytes) -> str:
 
 
 """
-Polynomials can be written in factored form: (x - a1)(x - a2)...(x - an)
-or in expanded form: b1*x^n + b2*x^n-1 + ... + bn
-This function can be used to find the coefficients within the expanded form
-of a polynomial given its factored form.
-Given the list of solutions to the polynomial when it is set to equal 0 
-(i.e. a1 ... an in the factored form above), this function returns the list
-of coefficients within the expanded form (b1 ... bn in the expanded form above)"""
-
-
+Polynomials can be written in factored form: (x - a1)(x - a2)...(x - an) or in expanded form:
+b1*x^n + b2*x^n-1 + ... + bn.
+This function can be used to find the coefficients within the expanded form of a polynomial given its factored form.
+Given the list of solutions to the polynomial when it is set to equal 0  (i.e. a1 ... an in the factored form above),
+this function returns the list of coefficients within the expanded form (b1 ... bn in the expanded form above).
+"""
 def get_polynomial_coefficients(numbers) -> list:
     coefficients = []
     for i in range(len(numbers), 0, -1):
@@ -280,12 +310,17 @@ def get_polynomial_coefficients(numbers) -> list:
         coefficients.append(total)
     return coefficients
 
+
 def calculate_polynomial(attributes, hi) -> int:
     Hs_b_coefficients = get_polynomial_coefficients(attributes)
     Hs_b_coefficients.append(1)
     return reduce(operator.mul, [hi[i] * Hs_b_coefficients[i] for i in range(len(Hs_b_coefficients))])
 
 
+"""
+Aggregate algorithm taken from the conference paper: Fully Collusion Secure Dynamic Broadcast Encryption with 
+Constant-Size Ciphertexts or Decryption Keys authored by Pascal Paillier and David Pointcheval.
+"""
 def aggregate(x_array, p_array) -> int:
     p_array = list(p_array)
     if len(x_array) != len(p_array):
